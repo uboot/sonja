e776d7a8c8553ec4adb22e34bf818273d4e95a4b
Matthias Fuchs <matz.fuchs@gmx.at>
2022-09-21 20:39:04 +0200

    Automatically crawl new repos

--- services/public/api/repo.py
+++ services/public/api/repo.py
@@ -3,14 +3,17 @@ from public.auth import get_read, get_write
 from public.schemas.repo import RepoReadItem, RepoReadList, RepoWriteItem
 from public.crud.repo import create_repo, delete_repo, read_repos, read_repo, update_repo
 from sonja.database import get_session, Session
+from sonja.client import Crawler
 
 router = APIRouter()
+crawler = Crawler()
 
 
 @router.post("/repo", response_model=RepoReadItem, response_model_by_alias=False, status_code=status.HTTP_201_CREATED)
 def post_repo_item(repo: RepoWriteItem, session: Session = Depends(get_session),
                    authorized: bool = Depends(get_write)):
     new_repo = create_repo(session, repo)
+    crawler.process_repo(new_repo.id)
     return RepoReadItem.from_db(new_repo)
 
 
