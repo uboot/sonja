1091319d17862c0b96edab06e18407dc390e57da
Matthias Fuchs <matz.fuchs@gmx.at>
2023-01-12 22:17:13 +0100

    Use POST for general API

--- services/public/test/api/test_general.py
+++ services/public/test/api/test_general.py
@@ -13,38 +13,38 @@ class TestGeneral(ApiTestCase):
         self.crawler_mock.reset_mock()
         self.redis_client_mock.reset_mock()
 
-    def test_get_clear_ecosystems(self):
+    def test_clear_ecosystems(self):
         run_create_operation(create_ecosystem, dict())
-        response = client.get(f"{api_prefix}/clear_ecosystems", headers=self.admin_headers)
+        response = client.post(f"{api_prefix}/clear_ecosystems", headers=self.admin_headers)
         self.assertEqual(200, response.status_code)
 
     def test_populate_database(self):
         run_create_operation(create_ecosystem, dict())
-        response = client.get(f"{api_prefix}/populate_database", headers=self.admin_headers)
+        response = client.post(f"{api_prefix}/populate_database", headers=self.admin_headers)
         self.assertEqual(200, response.status_code)
 
     def test_add_build(self):
         ecosystem_id = run_create_operation(create_ecosystem, dict())
         run_create_operation(create_profile, dict(), ecosystem_id)
         run_create_operation(create_commit, dict(), ecosystem_id)
-        response = client.get(f"{api_prefix}/add_build", headers=self.admin_headers)
+        response = client.post(f"{api_prefix}/add_build", headers=self.admin_headers)
         self.assertEqual(200, response.status_code)
         self.redis_client_mock.publish_build_update.assert_called_once()
 
     def test_add_run(self):
         run_create_operation(create_ecosystem, dict())
-        response = client.get(f"{api_prefix}/add_run", headers=self.admin_headers)
+        response = client.post(f"{api_prefix}/add_run", headers=self.admin_headers)
         self.assertEqual(200, response.status_code)
         self.redis_client_mock.publish_run_update.assert_called_once()
 
     def test_add_log_line(self):
         ecosystem_id = run_create_operation(create_ecosystem, dict())
         run_create_operation(create_run, dict(), ecosystem_id)
-        response = client.get(f"{api_prefix}/add_log_line", headers=self.admin_headers)
+        response = client.post(f"{api_prefix}/add_log_line", headers=self.admin_headers)
         self.assertEqual(200, response.status_code)
         self.redis_client_mock.publish_log_line_update.assert_called_once()
 
     def test_process_repo(self):
-        response = client.get(f"{api_prefix}/process_repo/1", headers=self.user_headers)
+        response = client.post(f"{api_prefix}/process_repo/1", headers=self.user_headers)
         self.assertEqual(200, response.status_code)
         self.crawler_mock.process_repo.assert_called_with("1")
